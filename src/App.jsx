a0b9c34352d3533b98dc852dabf9b4349041c21d
Andherson <00andherson@gmail.com>
2024-01-30 09:56:36 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { PrimeReactProvider } from "primereact/api";
+
+function App() {
+  return (
+    <PrimeReactProvider>
+      <>
+        <h1>Projeto Visitante</h1>
+      </>
+    </PrimeReactProvider>
+  );
+}
+
+export default App;
