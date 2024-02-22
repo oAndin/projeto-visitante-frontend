338cf3bd2922dc9883a6497737380bcd1a3e58ba
Andherson <00andherson@gmail.com>
2024-02-22 09:50:24 -0300

    updated

--- src/components/PieChart/Index.jsx
+++ src/components/PieChart/Index.jsx
@@ -4,34 +4,56 @@ import { Chart } from "primereact/chart";
 export default function PieChartDemo() {
   const [chartData, setChartData] = useState({});
   const [chartOptions, setChartOptions] = useState({});
-  const [data, setData] = useState([]);
 
-  useEffect(() => {}, []);
+  const fetchVisitantesGenero = () => {
+    fetch("http://localhost:5000/visitante")
+      .then((response) => response.json())
+      .then((visitantes) => {
+        const informacoes = {
+          masculino: 0,
+          feminino: 0,
+          outros: 0,
+        };
+        visitantes.map((v) => {
+          if (v.genero === "masculino") {
+            informacoes.masculino++;
+          } else if (v.genero === "feminino") {
+            informacoes.feminino++;
+          } else {
+            informacoes.outros++;
+          }
+        });
+        const data = {
+          labels: [
+            "masculino",
+            "feminino",
+            "outros",
+          ],
+          datasets: [
+            {
+              data: [informacoes.masculino, informacoes.feminino, informacoes.outros],
+              backgroundColor: ["#3fd5ffab", "#ff3f3fab", "#a83fffab"],
+              hoverBackgroundColor: ["#3fd5ff", "#ff3f3f", "#a83fff"],
+            },
+          ],
+        };
+        const options = {
+          plugins: {
+            legend: {
+              labels: {
+                usePointStyle: true,
+              },
+            },
+          },
+        };
+
+        setChartData(data);
+        setChartOptions(options);
+      });
+  };
 
   useEffect(() => {
-    const documentStyle = getComputedStyle(document.documentElement);
-    const data = {
-      labels: ["A", "B", "C"],
-      datasets: [
-        {
-          data: [540, 325, 702],
-          backgroundColor: ["#3fd5ffab", "#ff3f3fab", "#a83fffab"],
-          hoverBackgroundColor: ["#3fd5ff", "#ff3f3f", "#a83fff"],
-        },
-      ],
-    };
-    const options = {
-      plugins: {
-        legend: {
-          labels: {
-            usePointStyle: true,
-          },
-        },
-      },
-    };
-
-    setChartData(data);
-    setChartOptions(options);
+    fetchVisitantesGenero();
   }, []);
 
   return (
